8a70495314476e64fa9cc6c0bbcda94d4d95b4b1
joe4ffs <priiyahasan510@gmail.com>
2025-06-20 17:59:02 +0600

    Updated DUCSU Memory Match game with image paths, clues, and logic

--- memory_match/main.cpp
+++ memory_match/main.cpp
@@ -4,245 +4,227 @@
 #include <algorithm>
 #include <random>
 
-// === CONSTANTS ===
+using namespace std;
+
 const int screenWidth = 800;
 const int screenHeight = 600;
-const float maxRoundTime = 15.0f;
-const float showTime = 3.0f;
+const int cardRows = 3;
+const int cardCols = 4;
 
-// === GLOBAL STATE ===
-float roundTimer = 0.0f;
-float phaseTimer = 0.0f;
-bool inputEnabled = false;
+struct MemoryCard {
+    Rectangle rect;
+    string word;
+    bool revealed;
+    bool found;
+};
 
-Texture2D bgImage;
-Rectangle startButton = { 330, 500, 140, 50 };
+struct SnackCard {
+    Rectangle rect;
+    string snack;
+    bool revealed;
+    bool collected;
+    Texture2D image;
+};
 
-enum GamePhase { PHASE_START, PHASE_SHOES, PHASE_SNACKS, PHASE_CANDIDATE, PHASE_SUCCESS, PHASE_FAIL };
+vector<MemoryCard> cards;
+vector<SnackCard> snacks;
+vector<string> memoryWords = {
+    "Students", "Unite", "Against", "Corruption",
+    "Demand", "Fair", "Vote", "Rights",
+    "Protest", "Peace", "Justice", "Now"
+};
+
+vector<string> correctSnackTargets = {
+    "Juice", "Burger", "Cat", "Girl", "Mirror", "DUCSU"
+};
+
+vector<string> distractorSnacks = {
+    "Boy", "Bun", "Croissant", "DUSCU", "Water", "Cake"
+};
+
+vector<string> selectedClueWords;
+string cluePhrase = "";
+int snackAttempts = 0;
+bool allSnacksCollected = false;
+bool correctAnswerEntered = false;
+
+string poeticClue = "A girl and her cat, through DUCSU's glass dream,\nSaw juice and burger disrupt the mirror’s gleam.";
+string finalClueLine = "The one in the glass was always the key, DUCSU awaits, unknowingly thee.";
+string finalQuestion = "Who will save you now?";
+string correctFinalAnswer = "myself";
+string typewriterText = "";
+int typewriterIndex = 0;
+float typewriterTimer = 0.0f;
+float typewriterSpeed = 0.05f;
+
+string shuffledHint = "cat burger mirror DUCSU juice girl";
+string playerInput = "";
+bool answerPrompt = false;
+bool inputActive = false;
+bool gameWon = false;
+
+enum GamePhase {
+    PHASE_START,
+    PHASE_CARDS,
+    PHASE_SNACKS,
+    PHASE_CANDIDATE,
+    PHASE_INPUT_FINAL,
+    PHASE_SUCCESS,
+    PHASE_FAIL
+};
+
 GamePhase currentPhase = PHASE_START;
+float revealTime = 2.0f;
+float phaseTimer = 0.0f;
+Texture2D bgImage;
+Texture2D snackIcons[12];
+Rectangle startButton = { 330, 500, 140, 50 };
+Rectangle nextButton = { 500, 500, 140, 50 };
+Rectangle restartButton = { 160, 500, 140, 50 };
 
-// === STRUCTS ===
-struct Shoe {
-    Rectangle rect;
-    Color color;
-};
-std::vector<Shoe> shoes;
-int missingShoeIndex = 0;
+void ShuffleCards() {
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(memoryWords.begin(), memoryWords.end(), g);
 
-struct SnackBox {
-    Rectangle rect;
-    std::string content;
-};
-std::vector<SnackBox> snackBoxes;
-int correctSnackIndex = 0;
-
-float revealAlpha = 1.0f;
-void AnimateSnackBoxes(float delta) {
-    if (revealAlpha > 0.0f) revealAlpha -= delta * 0.3f;
-}
-
-void DrawSnackBoxes(bool showContent) {
-    for (auto& b : snackBoxes) {
-        DrawRectangleRec(b.rect, BROWN);
-        if (showContent)
-            DrawText(b.content.c_str(), b.rect.x + 10, b.rect.y + 20, 14, WHITE);
-        else
-            DrawRectangleLinesEx(b.rect, 2, WHITE);
+    cards.clear();
+    int index = 0;
+    for (int y = 0; y < cardRows; y++) {
+        for (int x = 0; x < cardCols; x++) {
+            MemoryCard card;
+            card.rect = { 80 + x * 150.0f, 120 + y * 120.0f, 120, 60 };
+            card.word = memoryWords[index++];
+            card.revealed = false;
+            card.found = false;
+            cards.push_back(card);
+        }
     }
 }
 
-struct Candidate {
-    Rectangle rect;
-    std::string name;
-    std::string symbol;
-    Color color;
-};
-std::vector<Candidate> candidates;
-int correctCandidateIndex = 0;
+void ShuffleSnacks() {
+    vector<string> allSnacks = correctSnackTargets;
+    allSnacks.insert(allSnacks.end(), distractorSnacks.begin(), distractorSnacks.end());
 
-// === FORWARD DECLARATIONS ===
-void ShuffleLayout();
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(allSnacks.begin(), allSnacks.end(), g);
 
-void DrawGlow(Rectangle rect, Color color) {
-    for (int i = 10; i > 0; i--) {
-        DrawRectangleLinesEx({ rect.x - i, rect.y - i, rect.width + i * 2, rect.height + i * 2 }, 1, Fade(color, 0.05f));
+    vector<Vector2> positions;
+    for (int i = 0; i < 12; i++) {
+        positions.push_back({ 80 + (i % 4) * 160.0f, 160 + (i / 4) * 100.0f });
     }
+    shuffle(positions.begin(), positions.end(), g);
+
+    snacks.clear();
+    string imageNames[12] = {
+        "juice.png", "burger.png", "catto.png", "girl.png", "mirror.png", "ducsufont.png",
+        "boy.png", "buns.png", "croissant.png", "duscu.png", "water.png", "cake.png"
+    };
+
+    for (int i = 0; i < 12; i++) {
+        SnackCard s;
+        s.rect = { positions[i].x, positions[i].y, 120, 60 };
+        s.snack = allSnacks[i];
+        s.revealed = false;
+        s.collected = false;
+        snackIcons[i] = LoadTexture(TextFormat("/mnt/c/Users/User/Pictures/Camera Roll/depts image/%s", imageNames[i].c_str()));
+        s.image = snackIcons[i];
+        snacks.push_back(s);
+    }
+    snackAttempts = 0;
+    allSnacksCollected = false;
 }
 
 void logic_draw_memory_challenge() {
-    float delta = GetFrameTime();
     BeginDrawing();
-    ClearBackground(BLACK);
+    ClearBackground(RAYWHITE);
     DrawTexture(bgImage, 0, 0, WHITE);
+    DrawText("Memory Match - DUCSU", 20, 20, 28, DARKBLUE);
 
     Vector2 mouse = GetMousePosition();
 
     if (currentPhase == PHASE_START) {
-        DrawText("DUCSU Memory Match!", 240, 80, 30, WHITE);
-        DrawText("Remember and choose correctly in each round.", 180, 130, 20, LIGHTGRAY);
-        DrawRectangleRec(startButton, LIGHTGRAY);
-        DrawText("START", startButton.x + 30, startButton.y + 10, 20, BLACK);
-
+        DrawRectangleRec(startButton, GRAY);
+        DrawText("Start", startButton.x + 30, startButton.y + 15, 24, WHITE);
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(mouse, startButton)) {
-            ShuffleLayout();
-            currentPhase = PHASE_SHOES;
-            roundTimer = 0.0f;
-            inputEnabled = false;
+            currentPhase = PHASE_CARDS;
+            ShuffleCards();
         }
-    } else if (currentPhase == PHASE_SHOES) {
-        DrawText("Round 1: Which shoe is missing?", 220, 40, 20, YELLOW);
-        roundTimer += delta;
-        if (roundTimer >= showTime) inputEnabled = true;
-
-        for (int i = 0; i < (int)shoes.size(); i++) {
-            if (inputEnabled || i != missingShoeIndex) {
-                DrawRectangleRec(shoes[i].rect, shoes[i].color);
-                if (inputEnabled) DrawGlow(shoes[i].rect, shoes[i].color);
-            }
-        }
-
-        if (inputEnabled && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            for (int i = 0; i < (int)shoes.size(); i++) {
-                if (CheckCollisionPointRec(mouse, shoes[i].rect)) {
-                    if (i == missingShoeIndex) {
-                        ShuffleLayout();
-                        currentPhase = PHASE_SNACKS;
-                        roundTimer = 0.0f;
-                        phaseTimer = 0.0f;
-                        inputEnabled = false;
-                        revealAlpha = 1.0f;
-                    } else {
-                        currentPhase = PHASE_FAIL;
-                    }
+    }
+    else if (currentPhase == PHASE_CARDS) {
+        for (auto &card : cards) {
+            DrawRectangleRec(card.rect, card.revealed ? LIGHTGRAY : GRAY);
+            if (card.revealed || card.found) DrawText(card.word.c_str(), card.rect.x + 10, card.rect.y + 20, 16, DARKBLUE);
+            if (!card.revealed && !card.found && IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(mouse, card.rect)) {
+                card.revealed = true;
+                selectedClueWords.push_back(card.word);
+                ShuffleCards();
+                if (selectedClueWords.size() >= 12) {
+                    cluePhrase = poeticClue + "\n" + finalClueLine;
+                    currentPhase = PHASE_SNACKS;
+                    ShuffleSnacks();
                 }
             }
         }
-    } else if (currentPhase == PHASE_SNACKS) {
-        DrawText("Round 2: Which box had the shingara?", 200, 40, 20, ORANGE);
-        phaseTimer += delta;
-        AnimateSnackBoxes(delta);
-
-        if (phaseTimer < showTime) {
-            DrawSnackBoxes(true);
-        } else {
-            inputEnabled = true;
-            DrawSnackBoxes(false);
-
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                for (int i = 0; i < (int)snackBoxes.size(); i++) {
-                    if (CheckCollisionPointRec(mouse, snackBoxes[i].rect)) {
-                        if (i == correctSnackIndex) {
-                            ShuffleLayout();
-                            currentPhase = PHASE_CANDIDATE;
-                            roundTimer = 0.0f;
-                            inputEnabled = false;
-                        } else {
-                            currentPhase = PHASE_FAIL;
-                        }
-                    }
+    }
+    else if (currentPhase == PHASE_SNACKS) {
+        DrawText(cluePhrase.c_str(), 40, 40, 20, MAROON);
+        for (int i = 0; i < snacks.size(); i++) {
+            SnackCard &snack = snacks[i];
+            if (!snack.collected) DrawTexture(snack.image, snack.rect.x, snack.rect.y, WHITE);
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(mouse, snack.rect)) {
+                snackAttempts++;
+                if (find(correctSnackTargets.begin(), correctSnackTargets.end(), snack.snack) != correctSnackTargets.end()) {
+                    snack.collected = true;
                 }
             }
         }
-    } else if (currentPhase == PHASE_CANDIDATE) {
-        DrawText("Round 3: Who is the right candidate?", 220, 40, 20, SKYBLUE);
-        for (int i = 0; i < (int)candidates.size(); i++) {
-            DrawRectangleRec(candidates[i].rect, candidates[i].color);
-            DrawGlow(candidates[i].rect, candidates[i].color);
-            DrawText(candidates[i].name.c_str(), candidates[i].rect.x + 10, candidates[i].rect.y + 10, 18, BLACK);
-            DrawText(candidates[i].symbol.c_str(), candidates[i].rect.x + 40, candidates[i].rect.y + 40, 30, WHITE);
+        if (snackAttempts >= 10 || all_of(correctSnackTargets.begin(), correctSnackTargets.end(), [](const string &s) {
+            return any_of(snacks.begin(), snacks.end(), [&](SnackCard &sc) {
+                return sc.snack == s && sc.collected;
+            });
+        })) {
+            allSnacksCollected = true;
+            currentPhase = PHASE_INPUT_FINAL;
         }
-
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            for (int i = 0; i < (int)candidates.size(); i++) {
-                if (CheckCollisionPointRec(mouse, candidates[i].rect)) {
-                    if (i == correctCandidateIndex) {
-                        currentPhase = PHASE_SUCCESS;
-                    } else {
-                        currentPhase = PHASE_FAIL;
-                    }
-                }
+    }
+    else if (currentPhase == PHASE_INPUT_FINAL) {
+        DrawText(finalClueLine.c_str(), 80, 80, 20, DARKPURPLE);
+        DrawText(finalQuestion.c_str(), 80, 140, 22, BLACK);
+        DrawText(playerInput.c_str(), 80, 180, 24, BLUE);
+        if (IsKeyPressed(KEY_ENTER)) {
+            if (ToLower(playerInput) == correctFinalAnswer) {
+                correctAnswerEntered = true;
+                currentPhase = PHASE_SUCCESS;
+            } else {
+                currentPhase = PHASE_FAIL;
             }
         }
-    } else if (currentPhase == PHASE_SUCCESS) {
-        DrawText("You WIN! Clue Unlocked for Next Department!", 120, 260, 24, GREEN);
-        DrawRectangleRec(startButton, LIGHTGRAY);
-        DrawText("RESTART", startButton.x + 20, startButton.y + 10, 20, BLACK);
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(mouse, startButton)) {
-            ShuffleLayout();
-            currentPhase = PHASE_START;
-            roundTimer = 0.0f;
-            phaseTimer = 0.0f;
-            inputEnabled = false;
-        }
-    } else if (currentPhase == PHASE_FAIL) {
-        DrawText("Oops! Wrong choice. Try Again!", 200, 260, 24, RED);
-        DrawRectangleRec(startButton, LIGHTGRAY);
-        DrawText("RESTART", startButton.x + 20, startButton.y + 10, 20, BLACK);
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(mouse, startButton)) {
-            ShuffleLayout();
-            currentPhase = PHASE_START;
-            roundTimer = 0.0f;
-            phaseTimer = 0.0f;
-            inputEnabled = false;
-        }
+        int key = GetCharPressed();
+        if (key > 0 && isprint(key)) playerInput += (char)key;
+        if (IsKeyPressed(KEY_BACKSPACE) && !playerInput.empty()) playerInput.pop_back();
+    }
+    else if (currentPhase == PHASE_SUCCESS) {
+        DrawText("You discovered the truth. You are the candidate!", 100, 250, 24, GREEN);
+    }
+    else if (currentPhase == PHASE_FAIL) {
+        DrawText("Wrong answer. Try again.", 120, 250, 24, RED);
     }
 
     EndDrawing();
 }
 
-void ShuffleLayout() {
-    std::random_device rd;
-    std::mt19937 g(rd());
-
-    shoes.clear();
-    for (int i = 0; i < 6; i++) {
-        Shoe s;
-        s.rect = { 100 + (i % 3) * 200.0f + (float)GetRandomValue(-20, 20), 200 + (i / 3) * 120.0f + (float)GetRandomValue(-10, 10), 60, 60 };
-        s.color = Color{ (unsigned char)GetRandomValue(100, 255), (unsigned char)GetRandomValue(100, 255), (unsigned char)GetRandomValue(100, 255), 255 };
-        shoes.push_back(s);
-    }
-    missingShoeIndex = GetRandomValue(0, (int)shoes.size() - 1);
-
-    snackBoxes.clear();
-    const char* snacks[] = { "shingara", "samosa", "cake", "banana", "chips", "roll" };
-    std::vector<Vector2> positions;
-    for (int i = 0; i < 6; i++) positions.push_back({ 100 + (i % 3) * 200.0f + (float)GetRandomValue(-20, 20), 240 + (i / 3) * 110.0f + (float)GetRandomValue(-10, 10) });
-    std::shuffle(positions.begin(), positions.end(), g);
-    for (int i = 0; i < 6; i++) {
-        SnackBox box;
-        box.rect = { positions[i].x, positions[i].y, 80, 60 };
-        box.content = snacks[i % 6];
-        snackBoxes.push_back(box);
-    }
-    correctSnackIndex = GetRandomValue(0, (int)snackBoxes.size() - 1);
-    snackBoxes[correctSnackIndex].content = "shingara";
-
-    candidates.clear();
-    const char* names[] = { "Mehedi", "Tumpa", "Shuvo", "Jamal" };
-    const char* symbols[] = { "📢", "📚", "🐸", "🚩" };
-    for (int i = 0; i < 4; i++) {
-        Candidate c;
-        c.name = names[i];
-        c.symbol = symbols[i];
-        c.color = Color{ (unsigned char)GetRandomValue(80, 220), (unsigned char)GetRandomValue(80, 220), (unsigned char)GetRandomValue(80, 220), 255 };
-        c.rect = { 120 + i * 160.0f + (float)GetRandomValue(-15, 15), 300 + (float)GetRandomValue(-10, 10), 100, 100 };
-        candidates.push_back(c);
-    }
-    correctCandidateIndex = GetRandomValue(0, 3);
-}
-
 int main() {
     InitWindow(screenWidth, screenHeight, "DUCSU Memory Match");
-    InitAudioDevice();
     bgImage = LoadTexture("/mnt/c/Users/User/Pictures/Camera Roll/depts image/DUCSU.png");
     SetTargetFPS(60);
-    ShuffleLayout();
 
     while (!WindowShouldClose()) {
         logic_draw_memory_challenge();
     }
 
     UnloadTexture(bgImage);
+    for (int i = 0; i < 12; i++) UnloadTexture(snackIcons[i]);
     CloseWindow();
     return 0;
 }
